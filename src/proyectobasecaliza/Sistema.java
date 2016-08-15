cc45bac1501e5391835c7d75e9040985f7b0eac4
Iván Mera <ivan@cajanegra.com.ec>
2016-08-15 03:54:31 -0500

    Creacion de clase 'Sistema' para el manejo de funciones del programa

---
+++ src/proyectobasecaliza/Sistema.java
@@ -0,0 +1,12 @@
+package proyectobasecaliza;
+
+import java.sql.Connection;
+import java.sql.Statement;
+
+public abstract class Sistema {
+    static void validarUsuario(){
+        Connection conn;
+        Statement stmn;
+        String query;
+    }
+}
