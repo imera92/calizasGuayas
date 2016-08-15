fc4e267b606a97f64a04ae1bfb4b4579fc26cd72
Iván Mera <ivan@cajanegra.com.ec>
2016-08-15 10:35:27 -0500

    Se implemento la clase "Sitema" con las siguientes caracteristicas: - La clase manejara la funcionalidad logica del programa. Sus miembros y metodos seran estaticos. - Se implemento un metodo para cargar la conexion a la base, usuarios, etc (inicializarSistema) - Se implemento un metodo para cargar los usuarios de la base al programa (cargarUsuarios) - Se implemento un metodo para validar credenciales de usuario (validarUsuario)

--- src/proyectobasecaliza/Sistema.java
+++ src/proyectobasecaliza/Sistema.java
@@ -1,12 +1,55 @@
 package proyectobasecaliza;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import javax.swing.JTextField;
+import proyectobasecaliza.sistema.Usuario;
 
-public abstract class Sistema {
-    static void validarUsuario(){
-        Connection conn;
-        Statement stmn;
-        String query;
+public class Sistema {
+    private static MySqlAccess newAccess;
+    private static ArrayList<Usuario> usuarios;
+        
+    public static void incializarSistema(){
+        Sistema.newAccess = new MySqlAccess();
+        Sistema.newAccess.connection();
+        Sistema.usuarios = new ArrayList<Usuario>();
+        Sistema.cargarUsuarios();
     }
+    
+    public static void cerrar(){
+        Sistema.newAccess.closeConnection();
+    }
+    
+    public static MySqlAccess getNewAccess(){
+        return Sistema.newAccess;
+    }
+    
+    private static void cargarUsuarios(){
+        ResultSet rs;
+        try{
+            rs = Sistema.newAccess.query("SELECT * FROM usuarios");
+            while(rs.next()){
+                Usuario u = new Usuario();
+                u.setUser(rs.getString("user"));
+                u.setPass(rs.getString("pass"));
+                Sistema.usuarios.add(u);
+            }
+        }catch(SQLException se){
+            se.printStackTrace();
+        }
+    }
+    
+    public static boolean validarUsuario(JTextField user, JTextField pass){
+        Usuario uTemp = new Usuario();
+        uTemp.setUser(user.getText());
+        uTemp.setPass(pass.getText());
+        for(Usuario u : Sistema.usuarios){
+            if(u.compareTo(uTemp) == 1) return true;
+        }
+        return false;
+    }
+    
 }
