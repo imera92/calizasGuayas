d5a6286ca83942c7b28369cdc4fd31ab2961c915
Iván Mera <ivan@cajanegra.com.ec>
2016-08-17 02:12:20 -0500

    Cambio en el main para que el Look & Feel de la GUI se setee al del sistema al comenzar la ejecucion del programa

--- src/proyectobasecaliza/Main.java
+++ src/proyectobasecaliza/Main.java
@@ -1,16 +1,29 @@
 
 package proyectobasecaliza;
 
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
 public class Main {
     public static void main(String[] args) {
         Sistema.incializarSistema();
-
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } 
+        catch (UnsupportedLookAndFeelException e) {
+           // handle exception
+        }
+        catch (ClassNotFoundException e) {
+           // handle exception
+        }
+        catch (InstantiationException e) {
+           // handle exception
+        }
+        catch (IllegalAccessException e) {
+           // handle exception
+        }
         VLogin ventana = new VLogin();
         ventana.setLocationRelativeTo(null);
         ventana.setVisible(true);
-
-//            VPrincipal v=new VPrincipal();
-//            v.setLocationRelativeTo(null);
-//            v.setVisible(true);
     }
 }
