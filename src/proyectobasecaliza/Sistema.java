a720ae5424d369490d624038792969c0a93124d8
Jorge <jorge_18link@hotmail.com>
2016-08-17 20:02:06 -0500

    creadas las funciones insert en sistema.java

--- src/proyectobasecaliza/Sistema.java
+++ src/proyectobasecaliza/Sistema.java
@@ -65,4 +65,24 @@ public class Sistema {
                    "VALUES ('"+idFactura.getText()+"', CURDATE(), '"+fechaVenci.getText()+"', '"+estado.getText()+"', "+sacosVen.getText()+", "+precioUni.getText()+", "+total.getText()+", '"+idProd.getText()+"', '"+rucClie.getText()+"', '"+ceduEmpl.getText()+"');";
         Sistema.newAccess.write(sql);
     }
+    
+    public static void insertProducto(JTextField idprod,JTextField stock,JTextField nombre,JTextField precio) throws SQLException{
+        String sql = "INSERT INTO producto " +
+                   "VALUES ('"+idprod.getText()+"', "+stock.getText()+", '"+nombre.getText()+"', "+precio.getText()+");";
+        Sistema.newAccess.write(sql);
+    }
+    
+    public static void insertRetencion(JTextField idreten,JTextField porcen,JTextField valorRete,JTextField idFact) throws SQLException{
+        String sql = "INSERT INTO retencion " +
+                   "VALUES ('"+idreten.getText()+"', "+porcen.getText()+", "+valorRete.getText()+", '"+idFact.getText()+"');";
+        Sistema.newAccess.write(sql);
+    }
+    
+    public static void insertPago(JTextField numComprb,JTextField fechaPago,JTextField numCheque,JTextField bancoCheque,JTextField bancoDepo,JTextField cuentaDepo,JTextField valorCancel,JTextField idFact,JTextField idFormaPago) throws SQLException{
+        String sql = "INSERT INTO pago " +
+                   "VALUES ('"+numComprb.getText()+"', '"+fechaPago.getText()+"', '"+numCheque.getText()+"', '"+bancoCheque.getText()+"', '"+bancoDepo.getText()+"', '"+cuentaDepo.getText()+"', "+valorCancel.getText()+", '"+idFact.getText()+"', '"+idFormaPago.getText()+"');";
+        Sistema.newAccess.write(sql);
+    }
+    
+    
 }
