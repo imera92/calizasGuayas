b7b2ab510483308b5fe6db5457f8e1a5cb01466d
Iván Mera <ivan@cajanegra.com.ec>
2016-09-03 15:53:38 -0500

    Cambios en llamada a stored procedure de VEliminarFactura

--- src/proyectobasecaliza/VEliminarFactura.java
+++ src/proyectobasecaliza/VEliminarFactura.java
@@ -54,13 +54,9 @@ public class VEliminarFactura extends javax.swing.JFrame {
         jLabel2.setText("Seleccione la factura a Eliminar");
         getContentPane().add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 90, -1, -1));
 
-        Connection cn = Sistema.getNewAccess().getConn();
         ResultSet rs=null;
-        CallableStatement mycall= null;
         try{
-            mycall = cn.prepareCall("{call allFact ()}");
-            mycall.execute();
-            rs=mycall.getResultSet();
+            rs=Sistema.getNewAccess().query2("{call allFact ()}");
         }catch(Exception e){
 
         }
